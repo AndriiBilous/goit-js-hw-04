5f8aa3ae5c4db4b2cfc5c961dfcb725c60f6b6b3
Andrii <111307811+Rakshase@users.noreply.github.com>
2024-01-17 21:02:49 -0600

    to check

---
+++ js/task-1.js
@@ -0,0 +1,33 @@
+function isEnoughCapacity(products, containerSize) {
+  //1) Перетворити обʼєкт на масив;
+  const arrays = Object.values(products);
+
+  //2) Створити змінні;
+  let total = 0;
+  let result;
+
+  //3) Перебрати масив через цикл;
+  for (let array of arrays) {
+    total += array;
+
+    //4) Суму порівняти з параметром containerSize;
+    result = total <= containerSize ? true : false;
+  }
+
+  //5) Повернути результат;
+  return result;
+}
+
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
+
+// if(total <= containerSize){
+//   result = true;
+// }else{
+//   result = false;
+// }
